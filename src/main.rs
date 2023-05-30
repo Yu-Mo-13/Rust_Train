0e6cb22fcc591b4db1dfa34b9c5ad3d70107231b
Yuya Motozono <motozono.tech@gmail.com>
2023-05-30 22:01:22 +0900

    modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let mut var1: i32 = 6;
+    let mut var2: i32 = 7;
+
+    while var1 * var2 > 0 {
+        // var1とvar2を1ずつ減らす
+        var1 -= 1;
+        var2 -= 1;
+        println!("var1={}, var2={}", var1, var2);
+        println!("Hello, world!");
+    }
 }
