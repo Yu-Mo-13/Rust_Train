6a76a69ddb4609a26d877517dc73300cbee2f149
Yuya Motozono <motozono.tech@gmail.com>
2023-06-05 22:46:14 +0900

    modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,28 @@
 fn main() {
     let mut var1: i32 = 6;
     let mut var2: i32 = 7;
+    let mut var3: i32;
 
     while var1 * var2 > 0 {
-        // var1とvar2を1ずつ減らす
-        var1 -= 1;
-        var2 -= 1;
-        println!("var1={}, var2={}", var1, var2);
+        var1 = multiple(var2) - var1;
+        var3 = shrink(var1, var2);
+        var2 = var1 - var3;
+        println!("var1={}, var2={}, var3={}", var1, var2, var3);
         println!("Hello, world!");
     }
+}
+
+fn multiple(x: i32) -> i32 {
+    if x < 0 {
+        return 0;
+    }
+    return x * x;
+}
+
+fn shrink(x: i32, y: i32) -> i32 {
+    let len: i32 = 10 * y;
+    if x < 0 {
+        return 0;
+    }
+    return x / len;
 }
